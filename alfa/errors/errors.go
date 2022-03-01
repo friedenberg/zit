d98a88e990ab8f65a0b3f37379033004c25cc90a
Sasha Friedenberg <181492+friedenberg@users.noreply.github.com>
2022-03-01 11:24:40 -0500

    add errors.Is

--- alfa/errors/errors.go
+++ alfa/errors/errors.go
@@ -6,6 +6,10 @@ func As(err error, target interface{}) bool {
 	return xerrors.As(err, target)
 }
 
+func Is(err, target error) bool {
+	return xerrors.Is(err, target)
+}
+
 func PanicIfError(err interface{}) {
 	if err == nil {
 		return
