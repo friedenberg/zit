c2bdccc93d5035d5effcefc616485aa069566603
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2022-11-02 10:49:32 -0400

    update

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -1,8 +1,9 @@
 #! /bin/bash -e
 
+#TODO don't reset options on lazy build?
 rm -rf build_options
 gmake
-go clean --cache
+go clean -cache -fuzzcache
 git add .
 git commit -m update || true
 git push
