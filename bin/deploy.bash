40f633130bb572475490622faad2f62484649eea
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2022-11-02 11:06:39 -0400

    update

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -1,9 +1,19 @@
 #! /bin/bash -e
 
-#TODO don't reset options on lazy build?
-rm -rf build_options
+declare t
+
+if [[ -d build_options ]]; then
+	t="$(mktemp -d)"
+	mv build_options "$t"
+fi
+
+#TODO pause mr-build-and-watch and then resume after
 gmake
 go clean -cache -fuzzcache
 git add .
 git commit -m update || true
 git push
+
+if [[ -d "$t" ]]; then
+	mv "$t" build_options
+fi
