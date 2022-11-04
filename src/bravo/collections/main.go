68fd9f3947c2f209d39359cf7109c09891665867
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2022-11-04 10:19:18 -0400

    update

--- src/bravo/collections/main.go
+++ src/bravo/collections/main.go
@@ -5,22 +5,35 @@ import (
 	"fmt"
 )
 
-type ProtoObjekte interface {
-	fmt.Stringer
-}
-
-type ProtoObjektePointer interface {
-	flag.Value
-}
+//   _____
+//  |  ___|   _ _ __   ___ ___
+//  | |_ | | | | '_ \ / __/ __|
+//  |  _|| |_| | | | | (__\__ \
+//  |_|   \__,_|_| |_|\___|___/
+//
 
 type WriterFunc[T any] func(T) error
 type WriterFuncWithKey[T any] func(string, T) error
 type WriterFuncKey func(string) error
 
+//   ____             _
+//  |  _ \ ___   ___ | |___
+//  | |_) / _ \ / _ \| / __|
+//  |  __/ (_) | (_) | \__ \
+//  |_|   \___/ \___/|_|___/
+//
+
 type PoolLike[T any] interface {
 	Get() *T
 	Put(i *T) (err error)
 }
+
+//   ____       _
+//  / ___|  ___| |_ ___
+//  \___ \ / _ \ __/ __|
+//   ___) |  __/ |_\__ \
+//  |____/ \___|\__|___/
+//
 
 type SetLike[T any] interface {
 	Len() int
@@ -38,6 +51,21 @@ type MutableSetLike[T any] interface {
 	Del(T) error
 	DelKey(string) error
 	Reset(SetLike[T])
+}
+
+//  __     __    _              ____       _
+//  \ \   / /_ _| |_   _  ___  / ___|  ___| |_ ___
+//   \ \ / / _` | | | | |/ _ \ \___ \ / _ \ __/ __|
+//    \ V / (_| | | |_| |  __/  ___) |  __/ |_\__ \
+//     \_/ \__,_|_|\__,_|\___| |____/ \___|\__|___/
+//
+
+type ProtoObjekte interface {
+	fmt.Stringer
+}
+
+type ProtoObjektePointer interface {
+	flag.Value
 }
 
 type ValueSetLike[T flag.Value] interface {
