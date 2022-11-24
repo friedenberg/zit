97bc52e245d20922b756a17a3483909ac0fd850e
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2022-11-24 12:24:49 -0500

    update

--- src/golf/typ/set.go
+++
@@ -1,8 +0,0 @@
-package typ
-
-import (
-	"github.com/friedenberg/zit/src/bravo/collections"
-	"github.com/friedenberg/zit/src/delta/kennung"
-)
-
-type Set = collections.ValueSet[kennung.Typ, *kennung.Typ]
