b9c814bec55387a4a4f47631edfcd9fa47c16927
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2022-12-18 11:49:31 -0500

    update

--- bin/refactor.bash
+++ bin/refactor.bash
@@ -1,13 +1,15 @@
-#! /bin/bash -e
+#! /bin/bash -xe
 
 old="$1"
 new="$2"
 package="$3"
+super_package="$4"
 
 # file_diff="$(mktemp)"
 
 (
 	go_refactor_args=(
+    "-w"
 		"-r"
 		"$package.$old -> $package.$new"
 	)
@@ -17,9 +19,10 @@ package="$3"
 
 (
 	go_refactor_args=(
+    "-w"
 		"-r"
 		"$old -> $package"
 	)
 
-	gofmt "${go_refactor_args[@]}" "src/$package"
+	gofmt "${go_refactor_args[@]}" "src/$super_package/$package"
 )
