e0d9b8b128ee4875d02bbbe7ae0ff07332692306
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2023-04-05 11:29:37 -0400

    update

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -3,13 +3,13 @@
 if [[ "$(($(ag skip zz-tests_bats/ -l | wc -l)))" -gt 0 ]]; then
 	echo "The following tests have skips and so the deploy cannot go forward" >&2
 	ag skip zz-tests_bats/ -l
-  exit 1
+	exit 1
 fi
 
 if [[ "$(($(ag "log.Debug" -l src/ | wc -l)))" -gt 0 ]]; then
 	echo "The following files have debug logs and so the deploy cannot go forward" >&2
 	ag "log.Debug" -l src/
-  exit 1
+	exit 1
 fi
 
 git pull --rebase
@@ -24,6 +24,7 @@ else
 fi
 
 $cmd_make build/deploy
+make install
 
 go clean -cache -fuzzcache
 git add .
@@ -33,4 +34,3 @@ if [[ "$(git status --porcelain=v1 2>/dev/null | wc -l)" -gt 0 ]]; then
 fi
 
 git push
-make install
