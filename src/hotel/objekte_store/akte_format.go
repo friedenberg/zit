f0eff413471034231a4a9f4b3ebce71f51d468bd
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2023-04-22 21:23:37 -0400

    update

--- src/hotel/objekte_store/akte_format.go
+++ src/hotel/objekte_store/akte_format.go
@@ -3,8 +3,8 @@ package objekte_store
 import (
 	"io"
 
+	"github.com/friedenberg/zit/src/alfa/errors"
 	"github.com/friedenberg/zit/src/alfa/schnittstellen"
-	"github.com/friedenberg/zit/src/bravo/sha"
 	"github.com/friedenberg/zit/src/golf/objekte"
 )
 
@@ -32,30 +32,12 @@ func MakeAkteFormat[
 	}
 }
 
-// func (af akteFormat[O, OPtr]) Format(w io.Writer, e OPtr) (int64, error) {
-// 	return af.FormatSavedAkte(w, e.GetAkteSha())
-// }
-
-func (af akteFormat[O, OPtr]) ParseAkte(
-	r io.Reader,
-	e OPtr,
-) (n int64, err error) {
-	var sh schnittstellen.Sha
-
-	sh, n, err = af.ParseSaveAkte(r, e)
-
-	e.SetAkteSha(sha.Make(sh))
-
-	return
-}
-
 func (af akteFormat[O, OPtr]) FormatParsedAkte(
 	w io.Writer,
 	e O,
 ) (n int64, err error) {
 	if af.ParsedAkteFormatter == nil {
-		sh := e.GetAkteSha()
-		n, err = af.SavedAkteFormatter.FormatSavedAkte(w, sh)
+		err = errors.Errorf("no ParsedAkteFormatter")
 	} else {
 		n, err = af.ParsedAkteFormatter.FormatParsedAkte(w, e)
 	}
