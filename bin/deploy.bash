c398da7197e491bc051754af49f3a4a41d026a2f
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2023-08-20 08:49:58 -0400

    update

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -1,20 +1,19 @@
-#! /usr/bin/env bash -e
+#! /bin/bash -e
 
 ./bin/lint.bash
 
 git pull --rebase
 
 #TODO pause mr-build-and-watch and then resume after
-cmd_make=make
+cmd_make="make"
 
-if command -v gmake 2>&1 >/dev/null; then
+if command -v gmake >/dev/null 2>&1; then
   cmd_make=gmake
 else
   make
 fi
 
 $cmd_make build/deploy
-make install
 
 git add .
 
