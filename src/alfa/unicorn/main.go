d8621374d91223ee31d5b2e1c2040ca7d85e2777
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2023-12-08 14:38:36 -0500

    update

---
+++ src/alfa/unicorn/main.go
@@ -0,0 +1,24 @@
+package unicorn
+
+import "unicode/utf8"
+
+func Negate(f func(rune) bool) func(rune) bool {
+	return func(r rune) bool {
+		return !f(r)
+	}
+}
+
+func CountRune(b []byte, r rune) (c int) {
+	for i, w := 0, 0; i < len(b); i += w {
+		runeValue, width := utf8.DecodeRune(b[i:])
+
+		if runeValue != r {
+			return
+		}
+
+		c++
+		w = width
+	}
+
+	return
+}
---
+++ src/foxtrot/kennung_fmt/string_format_reader_etiketten.go
@@ -0,0 +1,39 @@
+package kennung_fmt
+
+import (
+	"github.com/friedenberg/zit/src/alfa/errors"
+	"github.com/friedenberg/zit/src/charlie/catgut"
+	"github.com/friedenberg/zit/src/charlie/collections_ptr"
+	"github.com/friedenberg/zit/src/echo/kennung"
+)
+
+type etikettenReader struct{}
+
+func MakeEtikettenReader() (f *etikettenReader) {
+	f = &etikettenReader{}
+
+	return
+}
+
+func (f *etikettenReader) ReadStringFormat(
+	rb *catgut.RingBuffer,
+	k kennung.EtikettMutableSet,
+) (n int64, err error) {
+	flag := collections_ptr.MakeFlagCommasFromExisting(
+		collections_ptr.SetterPolicyAppend,
+		k,
+	)
+
+	readable, ok, err := rb.PeekUpto('\n')
+
+	if !ok {
+		return
+	}
+
+	if err = flag.Set(readable.String()); err != nil {
+		errors.Wrap(err)
+		return
+	}
+
+	return
+}
