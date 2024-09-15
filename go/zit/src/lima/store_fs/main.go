a5cffdc0e7c1e27fee59190d3c6e1810470940da
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-09-15 13:32:01 -0400

    update

--- go/zit/src/lima/store_fs/main.go
+++ go/zit/src/lima/store_fs/main.go
@@ -51,25 +51,17 @@ func (fs *Store) GetExternalStoreLike() external_store.StoreLike {
 func (fs *Store) DeleteExternalLike(el sku.ExternalLike) (err error) {
 	e := el.(*External)
 
+	var i Item
+
+	if err = i.ReadFromExternal(e); err != nil {
+		err = errors.Wrap(err)
+		return
+	}
+
 	fs.deleteLock.Lock()
 	defer fs.deleteLock.Unlock()
 
-	if err = fs.deleted.Add(&e.item.Conflict); err != nil {
-		err = errors.Wrap(err)
-		return
-	}
-
-	if err = fs.deleted.Add(e.GetObjectFD()); err != nil {
-		err = errors.Wrap(err)
-		return
-	}
-
-	if err = fs.deleted.Add(e.GetBlobFD()); err != nil {
-		err = errors.Wrap(err)
-		return
-	}
-
-	if err = e.item.MutableSetLike.Each(fs.deleted.Add); err != nil {
+	if err = i.MutableSetLike.Each(fs.deleted.Add); err != nil {
 		err = errors.Wrap(err)
 		return
 	}
