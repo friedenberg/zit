253edbaf3f7c95a2361dea7f0545003e73cc77bd
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-01-16 12:44:55 -0500

    update

--- go/zit/src/quebec/commands/command.go
+++ go/zit/src/quebec/commands/command.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"flag"
-	"fmt"
 
 	"code.linenisgreat.com/zit/go/zit/src/golf/command"
 	"code.linenisgreat.com/zit/go/zit/src/november/local_working_copy"
@@ -27,38 +26,19 @@ func Commands() map[string]CommandOld {
 	return commands
 }
 
-func registerCommand(name string, cmd Command) {
-	registerCommandOld(name, cmd)
-}
-
-func registerCommandOld(n string, commandOrCommandBuildFunc any) {
+func registerCommand(n string, cmd Command) {
 	f := flag.NewFlagSet(n, flag.ExitOnError)
 
 	if _, ok := commands[n]; ok {
 		panic("command added more than once: " + n)
 	}
 
-	switch cmd := commandOrCommandBuildFunc.(type) {
-	case Command:
-		wrapper := command.Wrapper{
-			FlagSet: f,
-			Command: cmd,
-		}
-
-		wrapper.SetFlagSet(f)
-
-		commands[n] = wrapper
-
-	case func(*flag.FlagSet) CommandOld:
-		commands[n] = cmd(f)
-
-	case func(*flag.FlagSet) WithLocalWorkingCopy:
-		commands[n] = commandWithLocalWorkingCopy{
-			Command: cmd(f),
-			FlagSet: f,
-		}
-
-	default:
-		panic(fmt.Sprintf("command or command build func not supported: %T", cmd))
+	wrapper := command.Wrapper{
+		FlagSet: f,
+		Command: cmd,
 	}
+
+	wrapper.SetFlagSet(f)
+
+	commands[n] = wrapper
 }
