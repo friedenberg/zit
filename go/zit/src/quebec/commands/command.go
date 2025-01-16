74eeb027a381bf281aaeae97916cafdca145972f
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-01-16 12:48:49 -0500

    update

--- go/zit/src/quebec/commands/command.go
+++ go/zit/src/quebec/commands/command.go
@@ -1,25 +1,5 @@
 package commands
 
-import (
-	"flag"
-
-	"code.linenisgreat.com/zit/go/zit/src/golf/command"
-	"code.linenisgreat.com/zit/go/zit/src/november/local_working_copy"
-)
-
-type (
-	CommandOld interface {
-		GetFlagSet() *flag.FlagSet
-		Run(command.Dep)
-	}
-
-	Command = command.Command
-)
-
-type CompleteWithRepo interface {
-	Complete(u *local_working_copy.Repo, args ...string)
-}
-
 var commands = map[string]Command{}
 
 func Commands() map[string]Command {
@@ -27,18 +7,9 @@ func Commands() map[string]Command {
 }
 
 func registerCommand(n string, cmd Command) {
-	f := flag.NewFlagSet(n, flag.ExitOnError)
-
 	if _, ok := commands[n]; ok {
 		panic("command added more than once: " + n)
 	}
 
-	wrapper := command.Wrapper{
-		FlagSet: f,
-		Command: cmd,
-	}
-
-	wrapper.SetFlagSet(f)
-
-	commands[n] = wrapper
+	commands[n] = cmd
 }
