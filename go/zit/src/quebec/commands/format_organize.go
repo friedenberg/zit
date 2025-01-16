bd72686429cad6e94ac7c941775eeb8b4ea09c03
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-01-16 11:54:07 -0500

    update

--- go/zit/src/quebec/commands/format_organize.go
+++ go/zit/src/quebec/commands/format_organize.go
@@ -8,41 +8,45 @@ import (
 	"code.linenisgreat.com/zit/go/zit/src/charlie/files"
 	"code.linenisgreat.com/zit/go/zit/src/echo/fd"
 	"code.linenisgreat.com/zit/go/zit/src/echo/ids"
+	"code.linenisgreat.com/zit/go/zit/src/golf/command"
 	"code.linenisgreat.com/zit/go/zit/src/lima/organize_text"
-	"code.linenisgreat.com/zit/go/zit/src/november/local_working_copy"
+	"code.linenisgreat.com/zit/go/zit/src/papa/command_components"
 	"code.linenisgreat.com/zit/go/zit/src/papa/user_ops"
 )
 
-type FormatOrganize struct {
-	Flags organize_text.Flags
-}
-
 func init() {
-	registerCommandOld(
+	registerCommand(
 		"format-organize",
-		func(f *flag.FlagSet) WithLocalWorkingCopy {
-			c := &FormatOrganize{
-				Flags: organize_text.MakeFlags(),
-			}
-
-			c.Flags.SetFlagSet(f)
-
-			return c
+		&FormatOrganize{
+			Flags: organize_text.MakeFlags(),
 		},
 	)
 }
 
-func (c *FormatOrganize) Run(u *local_working_copy.Repo, args ...string) {
-	c.Flags.Config = u.GetConfig()
+type FormatOrganize struct {
+	command_components.LocalWorkingCopy
+
+	Flags organize_text.Flags
+}
+
+func (cmd *FormatOrganize) SetFlagSet(f *flag.FlagSet) {
+	cmd.Flags.SetFlagSet(f)
+}
+
+func (cmd *FormatOrganize) Run(dep command.Dep) {
+	args := dep.Args()
+	localWorkingCopy := cmd.MakeLocalWorkingCopy(dep)
+
+	cmd.Flags.Config = localWorkingCopy.GetConfig()
 
 	if len(args) != 1 {
-		u.CancelWithErrorf("expected exactly one input argument")
+		localWorkingCopy.CancelWithErrorf("expected exactly one input argument")
 	}
 
 	var fdee fd.FD
 
 	if err := fdee.Set(args[0]); err != nil {
-		u.CancelWithError(err)
+		localWorkingCopy.CancelWithError(err)
 	}
 
 	var r io.Reader
@@ -56,13 +60,13 @@ func (c *FormatOrganize) Run(u *local_working_copy.Repo, args ...string) {
 			var err error
 
 			if f, err = files.Open(args[0]); err != nil {
-				u.CancelWithError(err)
+				localWorkingCopy.CancelWithError(err)
 			}
 		}
 
 		r = f
 
-		defer u.MustClose(f)
+		defer localWorkingCopy.MustClose(f)
 	}
 
 	var ot *organize_text.Text
@@ -75,27 +79,27 @@ func (c *FormatOrganize) Run(u *local_working_copy.Repo, args ...string) {
 		var err error
 
 		if ot, err = readOrganizeTextOp.Run(
-			u,
+			localWorkingCopy,
 			r,
 			organize_text.NewMetadata(repoId),
 		); err != nil {
-			u.CancelWithError(err)
+			localWorkingCopy.CancelWithError(err)
 		}
 	}
 
-	ot.Options = c.Flags.GetOptionsWithMetadata(
-		u.GetConfig().PrintOptions,
-		u.SkuFormatBoxCheckedOutNoColor(),
-		u.GetStore().GetAbbrStore().GetAbbr(),
-		u.GetExternalLikePoolForRepoId(repoId),
+	ot.Options = cmd.Flags.GetOptionsWithMetadata(
+		localWorkingCopy.GetConfig().PrintOptions,
+		localWorkingCopy.SkuFormatBoxCheckedOutNoColor(),
+		localWorkingCopy.GetStore().GetAbbrStore().GetAbbr(),
+		localWorkingCopy.GetExternalLikePoolForRepoId(repoId),
 		ot.Metadata,
 	)
 
 	if err := ot.Refine(); err != nil {
-		u.CancelWithError(err)
+		localWorkingCopy.CancelWithError(err)
 	}
 
 	if _, err := ot.WriteTo(os.Stdout); err != nil {
-		u.CancelWithError(err)
+		localWorkingCopy.CancelWithError(err)
 	}
 }
